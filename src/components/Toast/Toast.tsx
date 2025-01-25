68d27c5a572821c05179b0e968564152e8233836
MEGUMMY1 <cys10902@nate.com>
2025-01-26 00:48:05 +0900

    Style: Toast 스타일 수정

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -1,6 +1,6 @@
 import styles from "./Toast.module.scss";
-import IconComponent from "../Asset/Icon";
 import { useToast } from "@/utils/useToast";
+import Image from "next/image";
 
 export default function Toast() {
   const { toast } = useToast();
@@ -8,27 +8,33 @@ export default function Toast() {
   if (!toast.isShow) return null;
 
   let borderColor = "";
+  let iconPath = "";
 
   switch (toast.type) {
     case "success":
       borderColor = styles.success;
+      iconPath = "/icon/toast-success.svg";
       break;
     case "error":
       borderColor = styles.error;
+      iconPath = "/icon/toast-error.svg";
       break;
     case "warning":
       borderColor = styles.warning;
+      iconPath = "/icon/toast-warning.svg";
       break;
     case "information":
       borderColor = styles.information;
+      iconPath = "/icon/toast-information.svg";
       break;
     default:
       borderColor = "";
+      iconPath = "";
   }
 
   return (
     <div className={`${styles.toast} ${borderColor}`}>
-      <IconComponent name={toast.type} width={30} height={30} />
+      <Image src={iconPath} alt={toast.type} width={30} height={30} />
       {toast.message}
     </div>
   );
