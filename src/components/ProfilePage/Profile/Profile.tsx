28d8722ee3aed437afc6d52fa305be3551b0eff8
MEGUMMY1 <cys10902@nate.com>
2025-01-26 00:48:21 +0900

    Feat: 프로필 사진 등록 및 수정

--- src/components/ProfilePage/Profile/Profile.tsx
+++ src/components/ProfilePage/Profile/Profile.tsx
@@ -13,12 +13,25 @@ import { useToast } from "@/utils/useToast";
 import { authState } from "@/states/authState";
 import IconComponent from "@/components/Asset/Icon";
 import { modalState } from "@/states/modalState";
+import { useEffect, useState } from "react";
+import router from "next/router";
+import { postPresignedUrl } from "@/api/aws/postPresigned";
+import { useMutation } from "react-query";
+import { putProfileImage } from "@/api/users/putMeImage";
 
 export default function Profile({ isMyProfile, id }: ProfileProps) {
   const { isLoggedIn } = useRecoilValue(authState);
   const [, setModal] = useRecoilState(modalState);
+  const { data: myData } = useMyData();
   const { data: userData, refetch: refetchUserData } = useUserData(id);
+  const [profileImage, setProfileImage] = useState<string>("");
   const { showToast } = useToast();
+
+  useEffect(() => {
+    if (myData) {
+      setProfileImage(myData.image || "/image/default.svg");
+    }
+  }, [myData]);
 
   const handleFollowClick = async () => {
     try {
@@ -40,6 +53,58 @@ export default function Profile({ isMyProfile, id }: ProfileProps) {
 
   const handleOpenEditModal = () => {
     setModal({ isOpen: true, type: "PROFILE-EDIT", data: null });
+  };
+
+  const ImageMutation = useMutation((imageName: string) => putProfileImage(imageName));
+
+  const uploadImageToServer = async (file: File) => {
+    try {
+      const fileExt = file.name.split(".").pop()?.toLowerCase();
+
+      if (!fileExt || !["jpg", "jpeg", "png"].includes(fileExt)) {
+        showToast("JPG, JPEG, PNG 파일만 업로드 가능합니다.", "error");
+        return;
+      }
+      const ext = fileExt as "jpg" | "jpeg" | "png";
+
+      const data = await postPresignedUrl({
+        type: "profile",
+        ext,
+      });
+
+      ImageMutation.mutate(data.imageName);
+
+      const uploadResponse = await fetch(data.url, {
+        method: "PUT",
+        body: file,
+        headers: {
+          "Content-Type": file.type,
+        },
+      });
+
+      if (!uploadResponse.ok) {
+        throw new Error(`${uploadResponse.status}`);
+      }
+
+      showToast("프로필 사진이 변경되었습니다!", "success");
+    } catch (error) {
+      showToast("프로필 사진 업로드에 실패했습니다.", "error");
+    }
+  };
+
+  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+
+    try {
+      const imageUrl = URL.createObjectURL(file);
+      setProfileImage(imageUrl);
+
+      await uploadImageToServer(file);
+    } catch (error) {
+      setProfileImage(myData?.image || "/image/default.svg");
+      console.error("File change error:", error);
+    }
   };
 
   return (
@@ -70,21 +135,57 @@ export default function Profile({ isMyProfile, id }: ProfileProps) {
             }
           >
             {userData.image !== "https://image.grimity.com/null" ? (
-              <Image
-                src={userData.image}
-                width={96}
-                height={96}
-                alt="프로필 이미지"
-                className={styles.profileImage}
-              />
+              <div className={styles.profileImageContainer}>
+                <Image
+                  src={profileImage}
+                  width={96}
+                  height={96}
+                  alt="프로필 이미지"
+                  className={styles.profileImage}
+                />
+                <label htmlFor="upload-image">
+                  <Image
+                    src="/icon/edit-profile-image.svg"
+                    width={36}
+                    height={36}
+                    alt="프로필 이미지 수정"
+                    className={styles.addProfileImage}
+                  />
+                </label>
+                <input
+                  id="upload-image"
+                  type="file"
+                  accept="image/*"
+                  className={styles.hidden}
+                  onChange={handleFileChange}
+                />
+              </div>
             ) : (
-              <Image
-                src="/image/default.svg"
-                width={96}
-                height={96}
-                alt="프로필 이미지"
-                className={styles.profileImage}
-              />
+              <div className={styles.profileImageContainer}>
+                <Image
+                  src="/image/default.svg"
+                  width={96}
+                  height={96}
+                  alt="프로필 이미지"
+                  className={styles.profileImage}
+                />
+                <label htmlFor="upload-image">
+                  <Image
+                    src="/icon/add-profile-image.svg"
+                    width={36}
+                    height={36}
+                    alt="프로필 이미지 추가"
+                    className={styles.addProfileImage}
+                  />
+                </label>
+                <input
+                  id="upload-image"
+                  type="file"
+                  accept="image/*"
+                  className={styles.hidden}
+                  onChange={handleFileChange}
+                />
+              </div>
             )}
             <div className={styles.spaceBetween}>
               <div className={styles.leftContainer}>
