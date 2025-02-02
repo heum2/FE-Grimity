8d996a17eddb5d72a935bfacbb83243e0e189557
MEGUMMY1 <cys10902@nate.com>
2025-02-02 16:31:35 +0900

    Feat: 인기 유저 내 팔로우,언팔로우 로직 추가

--- src/components/PopularPage/PopularUser/User/User.tsx
+++ src/components/PopularPage/PopularUser/User/User.tsx
@@ -1,17 +1,45 @@
+import { useState } from "react";
 import { PopularResponse } from "@/api/users/getPopular";
 import styles from "./User.module.scss";
 import Image from "next/image";
 import Button from "@/components/Button/Button";
 import Link from "next/link";
+import { useToast } from "@/utils/useToast";
+import { putFollow } from "@/api/users/putIdFollow";
+import { deleteFollow } from "@/api/users/deleteIdFollow";
 
 export default function User({
   id,
   name,
   image,
-  followerCount,
-  isFollowing,
+  followerCount: initialFollowerCount,
+  isFollowing: initialIsFollowing,
   thumbnails,
 }: PopularResponse) {
+  const { showToast } = useToast();
+  const [isFollowing, setIsFollowing] = useState(initialIsFollowing);
+  const [followerCount, setFollowerCount] = useState(initialFollowerCount);
+
+  const handleFollowClick = async () => {
+    try {
+      await putFollow(id);
+      setIsFollowing(true);
+      setFollowerCount(followerCount + 1);
+    } catch (error) {
+      showToast("오류가 발생했습니다. 다시 시도해주세요.", "error");
+    }
+  };
+
+  const handleUnfollowClick = async () => {
+    try {
+      await deleteFollow(id);
+      setIsFollowing(false);
+      setFollowerCount(followerCount - 1);
+    } catch (error) {
+      showToast("오류가 발생했습니다. 다시 시도해주세요.", "error");
+    }
+  };
+
   return (
     <div className={styles.container}>
       <Link href={`/users/${id}`}>
@@ -81,11 +109,11 @@ export default function User({
           </div>
         </Link>
         {isFollowing ? (
-          <Button size="s" type="outlined-assistive">
+          <Button size="s" type="outlined-assistive" onClick={handleUnfollowClick}>
             팔로잉
           </Button>
         ) : (
-          <Button size="s" type="filled-primary">
+          <Button size="s" type="filled-primary" onClick={handleFollowClick}>
             팔로우
           </Button>
         )}
