c3302889f252f02249bc981c30629039bf1cfe04
MEGUMMY1 <cys10902@nate.com>
2025-02-03 23:26:32 +0900

    Fix: build error

--- src/api/users/getIdFeeds.ts
+++ src/api/users/getIdFeeds.ts
@@ -33,7 +33,7 @@ export async function getUserFeeds({
       nextCursor: response.data.nextCursor,
       feeds: response.data.feeds.map((feed: Feed) => ({
         ...feed,
-        cards: feed.cards.map((card: string) => `https://image.grimity.com/${card}`),
+        cards: feed.cards?.map((card: string) => `https://image.grimity.com/${card}`),
         thumbnail: `https://image.grimity.com/${feed.thumbnail}`,
       })),
     };
