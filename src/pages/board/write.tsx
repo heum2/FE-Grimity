dd87087af1d624d234f1a6caac5ea6bf95102e31
MEGUMMY1 <cys10902@nate.com>
2025-02-08 02:14:14 +0900

    Feat: 게시판 Editor 변경

--- src/pages/board/write.tsx
+++ src/pages/board/write.tsx
@@ -2,15 +2,13 @@ import { InitialPageMeta } from "@/components/MetaData/MetaData";
 import { serviceUrl } from "@/constants/serviceurl";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import dynamic from "next/dynamic.js";
+import BoardWrite from "@/components/Board/BoardWrite/BoardWrite";
 
 export default function BoardWritePage() {
   const router = useRouter();
   const [OGTitle] = useState("글 올리기 - 그리미티");
   const [OGUrl, setOGUrl] = useState(serviceUrl);
-  const BoardWrite = dynamic(() => import("@/components/Board/BoardWrite/BoardWrite"), {
-    ssr: false,
-  });
+
   useEffect(() => {
     setOGUrl(serviceUrl + router.asPath);
   }, [router.asPath]);
