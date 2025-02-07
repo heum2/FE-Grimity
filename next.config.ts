a35d0c62af3c010cc448366c49549938e901e1d3
MEGUMMY1 <cys10902@nate.com>
2025-02-07 20:09:15 +0900

    Fix: 의존성 충돌

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+};
+
+module.exports = {
+  images: {
+    domains: ["image.grimity.com"],
+  },
+};
+
+export default nextConfig;
