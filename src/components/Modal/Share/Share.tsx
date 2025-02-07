ca01b1fa4be9bda35639ca1b016b6a90d6ed71fc
MEGUMMY1 <cys10902@nate.com>
2025-02-08 00:20:57 +0900

    Fix: 공유 url 수정

--- src/components/Modal/Share/Share.tsx
+++ src/components/Modal/Share/Share.tsx
@@ -10,7 +10,7 @@ import { serviceUrl } from "@/constants/serviceurl";
 export default function Share({ feedId, title, image }: ShareBtnProps) {
   const { showToast } = useToast();
   const [, setModal] = useRecoilState(modalState);
-  const url = `${serviceUrl}/feeds/${feedId}`;
+  const url = `${serviceUrl}feeds/${feedId}`;
 
   const copyToClipboard = async () => {
     try {
