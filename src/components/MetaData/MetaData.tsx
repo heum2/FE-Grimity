840e386d5b38010413b96781be676e5b7432f7b1
MEGUMMY1 <cys10902@nate.com>
2025-02-12 20:46:09 +0900

    Docs: 트위터 메타 이미지 변경

--- src/components/MetaData/MetaData.tsx
+++ src/components/MetaData/MetaData.tsx
@@ -11,7 +11,10 @@ export function InitialPageMeta({ title, url }: InitialPageMetaProps) {
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content="그림쟁이들을 위한 그림 커뮤니티, 그리미티" />
-      <meta name="twitter:image" content="/image/grimity.png" />
+      <meta
+        name="twitter:image"
+        content="https://avatars.githubusercontent.com/u/194518500?s=200&v=4"
+      />
       <meta name="twitter:url" content={url ?? "https://www.grimity.com"} />
     </Head>
   );
