9f3ec5f4f816b8d1c670a40f7ebb1285e031c03b
MEGUMMY1 <cys10902@nate.com>
2025-02-14 03:51:24 +0900

    Feat: 그림 상세 페이지 스크롤 유지

--- src/pages/feeds/[id]/index.tsx
+++ src/pages/feeds/[id]/index.tsx
@@ -1,6 +1,7 @@
 import Detail from "@/components/Detail/Detail";
 import { InitialPageMeta } from "@/components/MetaData/MetaData";
 import { serviceUrl } from "@/constants/serviceurl";
+import { useScrollRestoration } from "@/hooks/useScrollRestoration";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
@@ -8,6 +9,14 @@ export default function FeedDetail() {
   const router = useRouter();
   const [OGTitle] = useState("그림 상세 - 그리미티");
   const [OGUrl, setOGUrl] = useState(serviceUrl);
+  const { restoreScrollPosition } = useScrollRestoration("details-scroll");
+
+  useEffect(() => {
+    if (sessionStorage.getItem("details-scroll") !== null) {
+      restoreScrollPosition();
+      sessionStorage.removeItem("details-scroll");
+    }
+  }, []);
 
   useEffect(() => {
     setOGUrl(serviceUrl + router.asPath);
