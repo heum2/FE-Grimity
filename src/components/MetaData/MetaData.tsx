6a259a6dcd12a57bffd21055a528b227c1db3a2d
MEGUMMY1 <cys10902@nate.com>
2025-02-20 18:28:43 +0900

    Docs: description metadata 추가

--- src/components/MetaData/MetaData.tsx
+++ src/components/MetaData/MetaData.tsx
@@ -7,6 +7,7 @@ export function InitialPageMeta({ title, url }: InitialPageMetaProps) {
     <>
       <Head>
         <title>{title}</title>
+        <meta name="description" content="그림쟁이들을 위한 그림 커뮤니티, 그리미티" />
         <meta property="og:title" content={title} />
         <meta property="og:description" content="그림쟁이들을 위한 그림 커뮤니티, 그리미티" />
         <meta property="og:image" content="/image/grimity_default.png" />
