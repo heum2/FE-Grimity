13b33d5254e76caaa9343f898594de17da717c22
abyss-s <lh3729@naver.com>
2025-03-20 16:13:00 +0900

    Feat: users/me에 url 필드 추가

--- src/api/users/putMe.ts
+++ src/api/users/putMe.ts
@@ -4,6 +4,7 @@ export interface MyInfoRequest {
   name: string;
   description: string;
   links: { linkName: string; link: string }[];
+  url: string;
 }
 
 export interface Response {
@@ -11,11 +12,17 @@ export interface Response {
   message: string;
 }
 
-export async function putMyInfo({ name, description, links }: MyInfoRequest): Promise<Response> {
+export async function putMyInfo({
+  name,
+  description,
+  links,
+  url,
+}: MyInfoRequest): Promise<Response> {
   const response = await axiosInstance.put("/users/me", {
     name,
     description,
     links,
+    url,
   });
   return response.data;
 }
