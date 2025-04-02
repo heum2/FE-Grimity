9df01aa2765e1b0cf5bbe4b38abfa50d102dd66b
abyss-s <lh3729@naver.com>
2025-04-02 15:06:17 +0900

    delete: Toast 메시지 삭제

--- src/hooks/usePreventRightClick.ts
+++ src/hooks/usePreventRightClick.ts
@@ -1,9 +1,7 @@
 import { useEffect, useRef } from "react";
-import { useToast } from "@/hooks/useToast";
 
 export function usePreventRightClick<T extends HTMLElement>() {
   const ref = useRef<T>(null);
-  const { showToast } = useToast();
 
   useEffect(() => {
     const element = ref.current;
@@ -11,7 +9,6 @@ export function usePreventRightClick<T extends HTMLElement>() {
 
     const disableRightClick = (e: MouseEvent) => {
       e.preventDefault();
-      showToast("우클릭이 차단되었습니다.", "information");
     };
 
     element.addEventListener("contextmenu", disableRightClick);
@@ -19,7 +16,7 @@ export function usePreventRightClick<T extends HTMLElement>() {
     return () => {
       element.removeEventListener("contextmenu", disableRightClick);
     };
-  }, [showToast]);
+  });
 
   return ref;
 }
