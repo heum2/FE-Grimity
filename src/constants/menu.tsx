b654ef9f87961e6ea815bc1086e256a7e06edf36
abyss-s <lh3729@naver.com>
2025-04-14 15:00:30 +0900

    Refactor: 태블릿 사이드바 텍스트 중앙정렬

--- src/constants/menu.tsx
+++ src/constants/menu.tsx
@@ -8,6 +8,6 @@ export interface MenuItem {
 
 export const MENU_ITEMS: MenuItem[] = [
   { icon: "home", label: "홈", route: "home" },
-  { icon: "popular", label: "인기그림", route: "popular" },
-  { icon: "board", label: "자유게시판", route: "board" },
+  { icon: "popular", label: "인기\n그림", route: "popular" },
+  { icon: "board", label: "자유\n게시판", route: "board" },
 ];
