6cdf4efa56b714c046a2eb342241a34bbb3aac7b
abyss-s <lh3729@naver.com>
2025-04-16 09:31:13 +0900

    Fix: 배너 공지 서비스 링크로 수정

--- src/components/Layout/Banner/Banner.tsx
+++ src/components/Layout/Banner/Banner.tsx
@@ -4,32 +4,17 @@ import "swiper/css";
 import styles from "./Banner.module.scss";
 import Link from "next/link";
 import { useDeviceStore } from "@/states/deviceStore";
-import { baseUrl } from "@/constants/baseurl";
+import { serviceUrl } from "@/constants/serviceurl";
 
 export default function Banner() {
   const isMobile = useDeviceStore((state) => state.isMobile);
   const isTablet = useDeviceStore((state) => state.isTablet);
-  let mainBanner;
-
-  if (isMobile) {
-    mainBanner = (
-      <Link href={`${baseUrl}/posts/048ae290-4b1e-4292-9845-e4b2ca68ea6a`}>
-        <img src="/image/main-banner-m.png" loading="lazy" className={styles.banner} />
-      </Link>
-    );
-  } else if (isTablet) {
-    mainBanner = (
-      <Link href={`${baseUrl}/posts/048ae290-4b1e-4292-9845-e4b2ca68ea6a`}>
-        <img src="/image/main-banner-t.png" loading="lazy" className={styles.banner} />
-      </Link>
-    );
-  } else {
-    mainBanner = (
-      <Link href={`${baseUrl}/posts/048ae290-4b1e-4292-9845-e4b2ca68ea6a`}>
-        <img src="/image/main-banner.png" loading="lazy" className={styles.banner} />
-      </Link>
-    );
-  }
+  const noticeUrl = `${serviceUrl}/posts/048ae290-4b1e-4292-9845-e4b2ca68ea6a`;
+  const imageSrc = isMobile
+    ? "/image/main-banner-m.png"
+    : isTablet
+    ? "/image/main-banner-t.png"
+    : "/image/main-banner.png";
 
   return (
     <Swiper
@@ -40,7 +25,11 @@ export default function Banner() {
       loop={true}
       className={styles.bannerSwiper}
     >
-      <SwiperSlide>{mainBanner}</SwiperSlide>
+      <SwiperSlide>
+        <Link href={noticeUrl}>
+          <img src={imageSrc} loading="lazy" className={styles.banner} />
+        </Link>
+      </SwiperSlide>
     </Swiper>
   );
 }
