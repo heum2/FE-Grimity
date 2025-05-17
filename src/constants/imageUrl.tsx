8ee9f786e88e97c240f200fcc1d6821af7628b3e
abyss-s <lh3729@naver.com>
2025-05-18 02:40:24 +0900

    fix: #70 serviceUrl 붙이기

--- src/constants/imageUrl.tsx
+++ src/constants/imageUrl.tsx
@@ -1,3 +1,5 @@
+import { serviceUrl } from "./serviceurl";
+
 export const imageUrl = process.env.NEXT_PUBLIC_IMAGE_URL;
 
-export const DEFAULT_THUMBNAIL = "image/thumbnail-default.png";
+export const DEFAULT_THUMBNAIL = `${serviceUrl}/image/thumbnail-default.png`;
--- src/pages/posts/[id]/index.tsx
+++ src/pages/posts/[id]/index.tsx
@@ -1,5 +1,6 @@
 import { getSSRPostsDetails, PostBaseResponse } from "@/api/posts/getPostsId";
 import PostDetail from "@/components/Board/Detail/Detail";
+import { DEFAULT_THUMBNAIL } from "@/constants/imageUrl";
 import { serviceUrl } from "@/constants/serviceurl";
 import { GetServerSideProps } from "next";
 import Head from "next/head";
@@ -39,13 +40,13 @@ export default function FeedDetail({ details }: Props) {
         <meta name="description" content={details.content ?? ""} />
         <meta property="og:title" content={`${details.title} - 그리미티`} />
         <meta property="og:description" content={`${details.content} | grimity |`} />
-        <meta property="og:image" content={details.thumbnail ?? "/image/grimity_default.png"} />
+        <meta property="og:image" content={details.thumbnail ?? DEFAULT_THUMBNAIL} />
         <meta property="og:url" content={`${serviceUrl}/posts/${details.id}`} />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={`${details.title} - 그리미티`} />
-        <meta name="twitter:description" content={details.content ?? ""} />
-        <meta name="twitter:image" content={details.thumbnail ?? "/image/grimity_default.png"} />
+        <meta name="twitter:description" content={details.content ?? "grimity"} />
+        <meta name="twitter:image" content={details.thumbnail ?? DEFAULT_THUMBNAIL} />
       </Head>
       <PostDetail id={id as string} />
     </>
