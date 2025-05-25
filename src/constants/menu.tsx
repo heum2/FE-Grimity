5380cd90196326ee7c6b137903c9e0037309a4dd
abyss-s <lh3729@naver.com>
2025-05-25 20:18:50 +0900

    fix: 사이드바 home 라우팅 이중 슬래시 제거

--- src/constants/menu.tsx
+++ src/constants/menu.tsx
@@ -6,8 +6,8 @@ export interface MenuItem {
   route: string;
 }
 
-export const MENU_ITEMS: MenuItem[] = [
-  { icon: "home", label: "홈", route: "/" },
+export const MENU_ITEMS = [
+  { icon: "home", label: "홈", route: "" },
   { icon: "popular", label: "인기\n그림", route: "popular" },
   { icon: "board", label: "자유\n게시판", route: "board" },
 ];
