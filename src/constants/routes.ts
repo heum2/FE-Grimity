fc2c23208595fe33e2ba6665c335a0b0b944b905
heum2 <heum2.dev@gmail.com>
2025-05-28 22:00:17 +0900

    Feat: 경로 상수화

---
+++ src/constants/routes.ts
@@ -0,0 +1,10 @@
+const PATH_ROUTES = {
+  HOME: "/",
+  RANKING: "/ranking",
+  BOARD: "/board",
+  FOLLOWING: "/following",
+  FEEDS: "/feeds",
+  POST: "/posts",
+} as const;
+
+export { PATH_ROUTES };
