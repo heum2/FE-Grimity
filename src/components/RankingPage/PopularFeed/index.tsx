e107e8d84e28a29161157f7ad4e9d935a5373b1b
heum2 <heum2.dev@gmail.com>
2025-05-30 18:01:38 +0900

    Feat: 랭킹 UI 작업

--- src/components/RankingPage/PopularFeed/index.tsx
+++ src/components/RankingPage/PopularFeed/index.tsx
@@ -1,19 +1,32 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
+import DatePicker from "@/components/DatePicker";
+import TabView from "@/components/TabView";
 import Loader from "@/components/Layout/Loader/Loader";
 import Title from "@/components/Layout/Title/Title";
 import FeedCard from "@/components/RankingPage/PopularFeed";
 
 import { usePopularFeed } from "@/api/feeds/getPopular";
 
+import { DatePickerMode } from "@/components/DatePicker/DatePicker.types";
+
 import styles from "@/components/RankingPage/PopularFeed/PopularFeed.module.scss";
 
 export default function PopularFeed() {
+  const { pathname, query } = useRouter();
+
+  const mode = (query.mode as DatePickerMode) || DatePickerMode.WEEK;
+  const date = (query.date as string) ? new Date(query.date as string) : new Date();
+
+  const [activeTab, setActiveTab] = useState<DatePickerMode>(mode);
+  const [selectedDate, setSelectedDate] = useState<Date>(date);
+
+  const loadMoreRef = useRef<HTMLDivElement | null>(null);
+
   const { data, isLoading, fetchNextPage, hasNextPage, isFetchingNextPage, refetch } =
     usePopularFeed();
-  const loadMoreRef = useRef<HTMLDivElement | null>(null);
-  const { pathname } = useRouter();
+
   useEffect(() => {
     refetch();
   }, [pathname]);
@@ -48,6 +61,20 @@ export default function PopularFeed() {
       <div className={styles.titleContainer}>
         <Title>인기 그림 순위</Title>
       </div>
+
+      <TabView
+        tabs={[
+          { label: "주간", key: DatePickerMode.WEEK },
+          { label: "월간", key: DatePickerMode.MONTH },
+        ]}
+        activeTab={activeTab}
+        onTabChange={setActiveTab}
+      />
+
+      <div className={styles.datePickerContainer}>
+        <DatePicker mode={activeTab} selectedDate={selectedDate} onDateChange={setSelectedDate} />
+      </div>
+
       <section className={styles.feedContainer}>
         {data?.pages.map((page) => page.feeds.map((feed) => <FeedCard key={feed.id} {...feed} />))}
       </section>
