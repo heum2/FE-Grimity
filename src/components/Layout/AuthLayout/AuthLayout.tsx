9e08ba98f0b69383f52eb1be9403f4745935a859
heum2 <heum2.dev@gmail.com>
2025-07-13 19:19:05 +0900

    Fix: 헤더 - 로그인 모달 호출방식 변경

--- src/components/Layout/AuthLayout/AuthLayout.tsx
+++ src/components/Layout/AuthLayout/AuthLayout.tsx
@@ -3,20 +3,23 @@ import { useEffect, PropsWithChildren } from "react";
 import { useRouter } from "next/router";
 
 import { useAuthStore } from "@/states/authStore";
-import { useModalStore } from "@/states/modalStore";
+
+import { useModal } from "@/hooks/useModal";
 
 import Loader from "@/components/Layout/Loader/Loader";
+import Login from "@/components/Modal/Login/Login";
 
 export default function AuthLayout({ children }: PropsWithChildren) {
   const router = useRouter();
 
   const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
   const isAuthReady = useAuthStore((state) => state.isAuthReady);
-  const openModal = useModalStore((state) => state.openModal);
+
+  const { openModal } = useModal();
 
   useEffect(() => {
     if (isAuthReady && !isLoggedIn) {
-      openModal({ type: "LOGIN" });
+      openModal((close) => <Login close={close} />);
       router.push("/");
     }
   }, [isAuthReady, isLoggedIn, openModal, router]);
--- src/states/modalStore.ts
+++ src/states/modalStore.ts
@@ -2,7 +2,6 @@ import { ReactNode } from "react";
 import { create } from "zustand";
 
 export type ModalType =
-  | "LOGIN"
   | "NICKNAME"
   | "JOIN"
   | "PROFILE-ID"
