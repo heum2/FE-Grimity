95417dcd7257e22e226350868a43af623231314b
heum2 <heum2.dev@gmail.com>
2025-08-24 15:36:11 +0900

    Feat: 채팅 프로필 API 추가

---
+++ src/api/chats/getChatsUser.ts
@@ -0,0 +1,24 @@
+import axiosInstance from "@/constants/baseurl";
+import { UserBaseResponse } from "@grimity/dto";
+import { useQuery } from "@tanstack/react-query";
+import { AxiosResponse } from "axios";
+
+interface GetChatsUserRequest {
+  chatId: string;
+}
+
+export const getChatsUser = async ({ chatId }: GetChatsUserRequest) => {
+  const response = await axiosInstance.get<GetChatsUserRequest, AxiosResponse<UserBaseResponse>>(
+    `/chats/${chatId}/user`,
+  );
+
+  return response.data;
+};
+
+export const useGetChatsUser = (request: GetChatsUserRequest) => {
+  return useQuery({
+    queryKey: ["chats", request.chatId, "user"],
+    queryFn: () => getChatsUser(request),
+    enabled: !!request.chatId,
+  });
+};
