5310e983eea6f5306c4adc32fc75d980cea65363
heum2 <heum2.dev@gmail.com>
2025-11-17 18:04:49 +0900

    Fix: IntersectionObserver 를 이용해서 스크롤 시 이미지 데이터 요청

--- src/components/ResponsiveImage/ResponsiveImage.tsx
+++ src/components/ResponsiveImage/ResponsiveImage.tsx
@@ -1,15 +1,35 @@
-import { forwardRef } from "react";
+import { forwardRef, useImperativeHandle } from "react";
+
+import { useInViewImage } from "@/hooks/useInViewImage";
 
 import { ResponsiveImageProps } from "./ResponsiveImage.types";
 
 const ResponsiveImage = forwardRef<HTMLImageElement, ResponsiveImageProps>(
-  ({ src, alt = "", ...props }, ref) => {
+  ({ src, alt = "", ...props }, forwardedRef) => {
+    const { imgRef, imageSrc, isInView } = useInViewImage(src);
+    useImperativeHandle(forwardedRef, () => imgRef.current!);
+
+    const placeholder = 'data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg"%3E%3C/svg%3E';
+
     return (
       <picture>
-        <source media="(min-width: 1024px)" srcSet={`${src}?s=1200`} />
-        <source media="(min-width: 768px)" srcSet={`${src}?s=600`} />
-        <source media="(max-width: 767px)" srcSet={`${src}?s=300`} />
-        <img src={src} alt={alt} ref={ref} {...props} />
+        {isInView && (
+          <>
+            <source media="(min-width: 1024px)" srcSet={`${imageSrc}?s=1200`} />
+            <source media="(min-width: 768px)" srcSet={`${imageSrc}?s=600`} />
+            <source media="(max-width: 767px)" srcSet={`${imageSrc}?s=300`} />
+          </>
+        )}
+        <img
+          src={isInView ? imageSrc ?? placeholder : placeholder}
+          alt={alt}
+          ref={imgRef}
+          loading="lazy"
+          onError={(e) => {
+            e.currentTarget.src = placeholder;
+          }}
+          {...props}
+        />
       </picture>
     );
   },
