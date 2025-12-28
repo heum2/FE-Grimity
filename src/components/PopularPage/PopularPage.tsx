a909b7e215304789620cf8a28ef69279bf59ac5d
MEGUMMY1 <cys10902@nate.com>
2025-12-28 18:37:56 +0900

    fix: build error

--- src/components/PopularPage/PopularPage.tsx
+++ src/components/PopularPage/PopularPage.tsx
@@ -1,13 +1,7 @@
 import styles from "./PopularPage.module.scss";
-<<<<<<< HEAD
 import PopularTag from "@/components/RankingPage/PopularTag";
 import PopularUser from "@/components/RankingPage/PopularUser";
 import PopularFeed from "@/components/RankingPage/PopularFeed";
-=======
-import PopularTag from "./PopularTag/PopularTag";
-import PopularUser from "./PopularUser/PopularUser";
-import PopularFeed from "./PopularFeed/PopularFeed";
->>>>>>> 8f3813ed05c7aaca16a8ab9af6e620afc62903c5
 
 export default function Popular() {
   return (
